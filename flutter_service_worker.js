b7414c6811cf94b12721dd82c74ae13f3bbc8955
giapsoft <giapsoft@gmail.com>
2022-08-13 15:41:39 +0700

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,8 +33,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f0ccc7c07df83d089153f9f9525284c8",
-"/": "f0ccc7c07df83d089153f9f9525284c8",
+"index.html": "f9ac6852573b5ccd478d48c0883ebd75",
+"/": "f9ac6852573b5ccd478d48c0883ebd75",
 "main.dart.js": "8e5285b32a3fce30aa72acd30161c925",
 "manifest.json": "ab8fb113132ea5541c0cb9297b8743ae",
 "version.json": "99fe5c058b95ab9ea4db7656f76e5180"
